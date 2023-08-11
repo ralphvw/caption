8155ab47f995c7c67cf7f15044e03c7c0df22e88
ralphvw <williamsralph@enyata.com>
2023-08-11 13:53:54 +0000

    using wsgi

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def upload_image():
     return jsonify({'caption': caption})
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
